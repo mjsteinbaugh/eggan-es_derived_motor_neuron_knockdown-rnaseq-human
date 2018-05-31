f2700df70e92e4d254c73cd098c49606abd06f9b
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-31 12:14:41 -0400

    Update load script

--- load.R
+++ load.R
@@ -1,5 +1,7 @@
 library(bcbioRNASeq)
 bcb <- loadRNASeq(
-    uploadDir = file.path("illumina_rnaseq", "final")
+    uploadDir = file.path("illumina_rnaseq", "final"),
+    organism = "Homo sapiens",
+    genomeBuild = "GRCh37"
 )
 saveData(bcb, dir = file.path("data", Sys.Date()))
